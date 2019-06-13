d597d6abe9dd9839442d97e78f4d0a4ae6314034
Fecabr <fcantillob@gmail.com>
2019-06-12 21:47:54 -0600

    Primer
    Primer commit

---
+++ Assets/Scripts/MoveSnowMan.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSnowMan : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    private float vertical;
+    private float horizontal;
+
+    private Vector3 movement;
+
+    private float speed = 6f;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        Move(vertical,horizontal);
+        Turn(vertical,horizontal);
+	}
+
+    private void Move(float v, float h)
+    {
+        movement.Set(h,0,v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + movement);
+    }
+
+    void Turn(float v, float h)
+    {
+        movement.Set(h,0,v);
+        if (movement != Vector3.zero)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(movement);
+            rb.rotation = newRotation;
+        }
+    }
+
+}
---
+++ Assets/Scripts/MovementEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementEnemy : MonoBehaviour {
+
+    //Variable que determina los punto a y b .
+    public Transform[] waypoints;
+    private NavMeshAgent nav;
+    private int currentTargetIndex;
+
+    // Use this for initialization
+    void Start() {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Landed())
+        {
+            Nextpoint();
+        }
+        UpdateDestinationAgent(waypoints[currentTargetIndex]);
+	}
+    void Nextpoint() {
+        currentTargetIndex = (currentTargetIndex < waypoints.Length -1)? currentTargetIndex +1 : 0;
+    }
+    public void UpdateDestinationAgent(Transform target)
+    {
+        nav.destination = (target.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation,target.rotation, 0.05f);
+    }
+    private bool Landed()
+    {
+        return nav.remainingDistance <= nav.stoppingDistance && !nav.pathPending;
+    }
+}
