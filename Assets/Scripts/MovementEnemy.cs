867f0cf3fe9bf7002eb5050e8e2b84cfef9a440c
Fecabr <fcantillob@gmail.com>
2019-06-16 07:39:46 -0600

    Delete
    Se eliminar 2 enemigos los cuales no se tiene a un funcionalidad, por lo tanto se esta trabajabando solo con un GameObjete de enemigo.

--- Assets/Scripts/MovementEnemy.cs
+++ Assets/Scripts/MovementEnemy.cs
@@ -3,35 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MovementEnemy : MonoBehaviour {
+public class MovementEnemy : MonoBehaviour
+{
 
-    //Variable que determina los punto a y b .
     public Transform[] waypoints;
+
     private NavMeshAgent nav;
+
     private int currentTargetIndex;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         nav = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         if (Landed())
         {
-            Nextpoint();
+            NextPoint();
         }
+
         UpdateDestinationAgent(waypoints[currentTargetIndex]);
-	}
-    void Nextpoint() {
-        currentTargetIndex = (currentTargetIndex < waypoints.Length -1)? currentTargetIndex +1 : 0;
     }
+
     public void UpdateDestinationAgent(Transform target)
     {
         nav.destination = (target.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation,target.rotation, 0.05f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, 0.05f);
     }
-    private bool Landed()
+
+    public void NextPoint()
+    {
+        currentTargetIndex = (currentTargetIndex < waypoints.Length - 1) ? currentTargetIndex + 1 : 0;
+    }
+
+
+    public bool Landed()
     {
         return nav.remainingDistance <= nav.stoppingDistance && !nav.pathPending;
     }
