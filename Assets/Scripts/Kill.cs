d6b0e85d390d85ce8aa0f34a3d7df42adee91ad4
Fecabr <fcantillob@gmail.com>
2019-06-24 21:05:11 -0600

    Portales
    Se incluye tres portales, uno de los tres es para la muerte del GameObjete

---
+++ Assets/Scripts/Kill.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour {
+
+    public Transform portal;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = portal.transform.position;
+            other.transform.rotation = portal.transform.rotation;
+            Debug.Log("Perdiste....... ;(");
+        }
+    }
+}
