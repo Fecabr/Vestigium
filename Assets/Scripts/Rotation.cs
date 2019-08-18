38dacfbfaa0dcde80c18c5068d94a8be5fce46fe
Fecabr <fcantillob@gmail.com>
2019-08-17 21:48:04 -0600

    Update 0.5
    Se incluye el collider en el primer nivel

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+    // Use this for initialization
+    public float velocity;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up, Time.deltaTime * velocity, Space.World);
+    }
+}
